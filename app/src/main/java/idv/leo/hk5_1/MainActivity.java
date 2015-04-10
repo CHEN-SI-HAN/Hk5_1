0521b8f8a04a5e09bfd2e23b753d468ec6324278
CHEN-SI-NAN <leo200292@gmail.com>
2015-04-10 15:41:25 +0800

    Initial commit

---
+++ app/src/main/java/idv/leo/hk5_1/MainActivity.java
@@ -0,0 +1,157 @@
+package idv.leo.hk5_1;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.Handler;
+
+
+public class MainActivity extends ActionBarActivity {
+    private Handler aHandler;
+    private final static int NOTIFICATION_ID = 100;
+    private EditText editText;
+    private TextView tvResult;
+    private Button btn1, btn2;
+    private NotificationManager notificationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        aHandler = new Handler();
+        aHandler.post(runnable);
+
+        findViews();
+    }
+
+    Runnable runnable = new Runnable() {
+        public void run() {
+            int count = 2;
+
+            if (btn1.isClickable()) {
+                if (count > 0) {
+
+                    count--;
+                    aHandler.postDelayed(runnable, 1000);
+                }else{
+                    tvResult.setText(editText.getText().toString());
+                    /**
+                     what the fuck ?
+                     3秒等很久.................
+                     */
+                }
+
+            }
+        }
+    };
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        if (aHandler != null) {
+            aHandler.removeCallbacks(runnable);
+        }
+    }
+
+
+    private void findViews() {
+
+        editText = (EditText) findViewById(R.id.editText);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+
+                String title = getString(R.string.title);
+                String content = getString(R.string.content);
+                Intent intent = new Intent(MainActivity.this, NotiActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("title", title);
+                bundle.putString("content", content);
+                intent.putExtras(bundle);
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this
+                        , 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+                Notification notification = new Notification.Builder(MainActivity.this)
+
+                        .setTicker(getString(R.string.ticker))
+
+                        .setContentTitle(title)
+
+                        .setContentText(content)
+
+                        .setSmallIcon(R.drawable.ic_secret_notification)
+
+                        .setAutoCancel(true)
+
+                        .setContentIntent(pendingIntent)
+
+                        .setSound(soundUri)
+
+                        .addAction(R.drawable.ic_secret_notification, "View", pendingIntent)
+                        .addAction(0, "Remind", pendingIntent)
+                        .build();
+
+                notificationManager.notify(NOTIFICATION_ID, notification);
+
+
+            }
+        });
+
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                editText.setText("");
+                tvResult.setText(editText.getText().toString());
+                notificationManager.cancel(NOTIFICATION_ID);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
